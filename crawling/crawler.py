3891343d2e319facdf6c1cebc80e59d55efd5702
GODDONGHYUN <leenl1168@gmail.com>
2024-03-05 16:48:46 +0900

    2024-03-05 웹 크롤링 예시

--- crawling/crawler.py
+++ crawling/crawler.py
@@ -7,28 +7,105 @@
 # pip install beautifulsoup4
 # pip install selenium
 
-import requests
-from bs4 import BeautifulSoup
-
-URL = 'https://www.naver.com'
-
-response = requests.get(URL)
-print(response.text)
-
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    a_list = soup.find_all('a')
-    print(a_list)
-    print(soup)
-else:
-    print(response.status_code)
-
-
-
 # HTTP 상태코드 
 # 100번대 : 추가 요청 기다림
 # 200번대 : 성공
 # 300번대 : 리소스 위치 바뀜
 # 400번대 : 요청자 클라이언트 오류
 # 500번대 : 서버 오류  
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.naver.com'
+
+response = requests.get(URL)
+# print(response.text)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    a_list = soup.find_all('a')
+    # print(a_list)
+    # print(soup)
+else:
+    print(response.status_code)
+
+
+from selenium import webdriver 
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+
+time.sleep(2)
+
+driver.get(URL)
+time.sleep(2)
+
+search_input = driver.find_element(By.ID, 'query')
+search_input.send_keys('제네시스')
+time.sleep(1)
+search_input.send_keys(Keys.ENTER)
+time.sleep(2)
+
+news_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(8) > a')
+time.sleep(1)
+
+ActionChains(driver).click(news_button).perform()
+time.sleep(2)
+
+news_title_elements = driver.find_elements(By.CLASS_NAME, 'news_tit')
+
+time.sleep(1)
+
+for news_title_element in news_title_elements:
+    # title = news_title_element.text
+    title = news_title_element.get_attribute('title')
+    print(title)
+
+time.sleep(1)
+
+driver.back()
+time.sleep(3)
+
+image_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(1) > a')
+time.sleep(1)
+
+ActionChains(driver).click(image_button).perform()
+time.sleep(2)
+
+images_elements = driver.find_elements(By.CLASS_NAME, '_fe_image_tab_content_thumbnail_image')
+
+time.sleep(1)
+
+image_list = []
+
+for images_element in images_elements:
+    image_src = images_element.get_attribute('src')
+    print(image_src)
+    image_list.append(image_src)
+time.sleep(1)
+
+# 파이썬으로 폴더 생성
+import os 
+
+FOLDER_PATH = r'./images/'
+
+if not os.path.isdir(FOLDER_PATH):
+    os.mkdir(FOLDER_PATH)
+
+# 파이썬으로 폴더 생성//
+    
+
+# 파이썬으로 이미지 url 파일 다운로드
+from urllib.request import urlretrieve
+
+number = 1
+
+for image_src in image_list:
+    urlretrieve(image_src, FOLDER_PATH + f'{number}.png')
+    number += 1
+    time.sleep(0.5)
